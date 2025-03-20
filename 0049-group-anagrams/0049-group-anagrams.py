5e77fad61df0da691861d36a0a40dc431c5b2bc7
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-03-20 17:03:55 -0400

    Time: 26 ms (96.32%), Space: 18.3 MB (11.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        result = defaultdict(list)
+        result = defaultdict(list) # we use this because when there an empty list, it has no key, so by using the defauldict key default value is given
 
-        for s in strs:
+        for s in strs: # to loop though out the strs
             
-            count = [0]*26 # [0,0,0,,0,0,0,0,0,0,0,0,0,0..0]
-            for char in s:
-                count[ord(char)- ord("a")] += 1
-
-            result[tuple(count)].append(s)
+            count = [0]*26 # [0,0,0,,0,0,0,0,0,0,0,0,0,0..0] # we are created index value for each element as per alphabets 0-25
+            for char in s: # looping through the each character of the string
+                count[ord(char)- ord("a")] += 1 # so suppose s ="eat", char = 'e', count[101-97] += 1, after loop = [1,0,0,0,1,...1,0,0,0,0,0]
+ 
+            result[tuple(count)].append(s) #converting into tuple as immutable and unique key exits in dict
         return list(result.values())
         
