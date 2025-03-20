1fb9d3ff0bed7ad0a5a91a01b6f227243e5cf47b
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-03-20 05:20:06 -0400

    Time: 0 ms (100%), Space: 12.5 MB (27.55%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        
+        temp = strs[0]
+
+        for i in range(1, len(strs)):
+            while not strs[i].startswith(temp):
+                temp = temp[:-1]
+            if not temp:
+                return ""
+        
+        return temp
