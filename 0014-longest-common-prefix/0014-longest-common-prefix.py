15066414d709868265b156777abe0d6e28fb0ae1
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-03-20 05:24:41 -0400

    Time: 0 ms (100%), Space: 12.5 MB (27.55%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0:
-            return ""
+        if len(strs) == 0: # to check given list strs is empty or not.
+            return "" # if the case return empty string
         
-        temp = strs[0]
+        temp = strs[0] #take first element of the list to check with string in the the list
 
-        for i in range(1, len(strs)):
-            while not strs[i].startswith(temp):
-                temp = temp[:-1]
-            if not temp:
+        for i in range(1, len(strs)): # index starts with 1 to end of the given list strs
+            while not strs[i].startswith(temp): # need to cut down the string whenever the "i" value in list strs is not same as temp
+                temp = temp[:-1] 
+            if not temp: #if temp is empty string
                 return ""
         
         return temp
