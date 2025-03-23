097cc827c48a29a01ca4e5904553a4af7df1eb76
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-03-23 02:27:43 -0400

    Time: 0 ms (100%), Space: 12.4 MB (61%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,18 +1,19 @@
-class Solution(object):
+class Solution:
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0: # to check given list strs is empty or not.
-            return "" # if the case return empty string
-        
-        temp = strs[0] #take first element of the list to check with string in the the list
+        if not strs:
+            return ""
 
-        for i in range(1, len(strs)): # index starts with 1 to end of the given list strs
-            while not strs[i].startswith(temp): # need to cut down the string whenever the "i" value in list strs is not same as temp
-                temp = temp[:-1] 
-            if not temp: #if temp is empty string
-                return ""
-        
-        return temp
+        # Sort the array
+        strs.sort()
+
+        first, last = strs[0], strs[-1]
+        i = 0
+
+        while i < len(first) and i < len(last) and first[i] == last[i]:
+            i += 1
+
+        return first[:i]
