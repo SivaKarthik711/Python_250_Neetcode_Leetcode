0347ca9779bc8c2f84e7f1b5096d47da2186d666
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-03-23 01:16:21 -0400

    Time: 0 ms (100%), Space: 13 MB (89.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,13 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        dict_nums = {} # to check, we need index as return. So, index is the value for the dictionary
+        nums_map = {}
 
-        for index, i in enumerate(nums): # as we need both index to return and value to check, enumerate functions is helpful for this case which iterates over the list.
+        for index, value in enumerate(nums):
 
-            diff = target - nums[index] # we need a variable to check previous values in the dictionary
-            if diff in dict_nums:
-                return [dict_nums[diff],index]
+            diff = target - nums[index]
+            if diff in nums_map:
+                return [nums_map[diff],index]
             else:
-                dict_nums[i] = index
-        
+                nums_map[value] = index
